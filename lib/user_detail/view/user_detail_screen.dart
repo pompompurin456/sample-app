05c83c89c28a85f7f092aa3660595eb611644392
okuda <okuda.ryuya0765@gmail.com>
2022-08-27 00:59:52 +0900

    [update]Screenの実装

--- lib/user_detail/view/user_detail_screen.dart
+++ lib/user_detail/view/user_detail_screen.dart
@@ -1,27 +1,68 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sample/user_detail/provider/user_detail_provider.dart';
 
-class UserDetailScreen extends ConsumerWidget {
+class UserDetailScreen extends HookConsumerWidget {
   const UserDetailScreen({
     Key? key,
+    required this.userName,
     required this.userId,
   }) : super(key: key);
 
+  final String userName;
   final int userId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final userDetailAsyncValue = ref.watch(userDetailProvider(userId));
+    final sumBalance = ref.watch(sumBalanceProvider);
 
     return userDetailAsyncValue.when(
-      data: (userDetail) {
+      data: (userDetails) {
         return Scaffold(
           appBar: AppBar(
             title: const Text('Detail'),
           ),
-          body: Text(userDetail.first.name),
+          body: Column(
+            children: [
+              const SizedBox(height: 36),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Text('User:'),
+                  Text(userName),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Text('Total:'),
+                  Text(sumBalance.toString()),
+                ],
+              ),
+              const SizedBox(height: 48),
+              const Divider(color: Colors.black),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: userDetails.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final userDetail = userDetails[index];
+                    return Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                      child: ListTile(
+                        title: Text(userDetail.name),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         );
       },
       error: (error, stackTrace) {
