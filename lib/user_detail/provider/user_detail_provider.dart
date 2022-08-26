f9d33e63af055f8f9e8068ec8fd62ffad71890aa
okuda <okuda.ryuya0765@gmail.com>
2022-08-27 00:59:12 +0900

    [update]Providerの作成

--- lib/user_detail/provider/user_detail_provider.dart
+++ lib/user_detail/provider/user_detail_provider.dart
@@ -2,9 +2,17 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sample/api/entity/local/user_detail.dart';
 import 'package:sample/user_detail/repository/user_detail_repository.dart';
 
+final sumBalanceProvider = StateProvider<int>((ref) => 0);
+
 final userDetailProvider =
     FutureProvider.family<List<UserDetail>, int>((ref, userId) async {
-  return await ref
-      .read(userDetailRepositoryProvider)
-      .fetchUserDetail(id: userId);
+  final result =
+      await ref.watch(userDetailRepositoryProvider).fetchUserDetail(id: userId);
+
+  ref.read(sumBalanceProvider.notifier).state = result
+      .map((e) => e.balance)
+      .toList()
+      .reduce((value, element) => value + element);;
+
+  return result;
 });
